1de5aca99c6ec1cd21c25ddd04b9b71f13102204
Rahul Behera <rahulbehera2002@gmail.com>
2023-02-24 15:47:15 +0530

    feat: add MusicCardContainer component

---
+++ src/components/MusicCardContainer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './MusicCardContainer.css'
+
+function MusicCardContainer() {
+  return (
+    <div className='musicCardContainer'>
+      <p>:((</p>
+      <p>seems a bit empty in here...</p>
+      <button>sync</button>
+    </div>
+  )
+}
+
+export default MusicCardContainer;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Header from "./Header";
+import MusicCardContainer from "./MusicCardContainer";
+
+export { Header, MusicCardContainer };
