2d7d633c06e1dfb30cb860ff2bb215cd6ec9d053
Rahul Behera <rahulbehera2002@gmail.com>
2023-02-28 23:59:56 +0530

    fix: change fetch all like data

--- src/utils/likeMapperToMusic/index.js
+++ src/utils/likeMapperToMusic/index.js
@@ -3,11 +3,8 @@ import { GET_LIKES_BY_ID } from "../../constants/apiEndPoints";
 
 const likeMapperToMusic = async (musicData, navigate) => {
   try {
-    const newMusicData = [];
-    for(let i=0; i<musicData.length; ++i) {
-      const likes = await makeRequest(GET_LIKES_BY_ID(musicData[i].id), navigate);
-      newMusicData.push({ ...musicData[i], likes: likes.data });
-    };
+    const likesData = await Promise.all(musicData.map((singleMusicData) => makeRequest(GET_LIKES_BY_ID(singleMusicData.id), navigate) ))
+    const newMusicData = musicData.map((singleMusicData, index) => { return { ...singleMusicData, likes: likesData[index].data } });
     return newMusicData;
   } catch (error) {
     navigate(`/error/${error?.code}`);
